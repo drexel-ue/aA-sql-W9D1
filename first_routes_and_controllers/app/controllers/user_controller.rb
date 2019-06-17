774969357212c68fd06be2dcbfc2a77a0c7266da
ikesh pack <packappsforyou@gmail.com>
2019-06-17 17:43:44 -0400

    Wrired up user#show channel

--- first_routes_and_controllers/app/controllers/user_controller.rb
+++ first_routes_and_controllers/app/controllers/user_controller.rb
@@ -1,6 +1,12 @@
 class UserController < ApplicationController
-    def index
-        users = User.all
-        render json: users
-    end
+  def index
+      users = User.all
+      render json: users
+  end
+  
+  def show 
+    user = User.find(params[:id])
+     render json: user
+  end
+
 end
