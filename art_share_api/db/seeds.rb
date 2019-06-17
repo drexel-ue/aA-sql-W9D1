fb36a8f2031c976015ce1eea72dc8e052ef23748
ikesh pack <packappsforyou@gmail.com>
2019-06-17 19:06:34 -0400

    Altered users table to only include username, modified model and seed.rd to reflect

--- art_share_api/db/seeds.rb
+++ art_share_api/db/seeds.rb
@@ -7,19 +7,19 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 names = [
-    {'name' => 'Ilka', 'email' => 'Ilka@gnet.serve'},
-    {'name' => 'Ervin', 'email'  => 'Ervin@ydos.parse'},
-    {'name' => 'Davey', 'email'  => 'Davey@darvey.dave'},
-    {'name' =>'Desmund' , 'email' => 'Desmund@hesmund.centro'},
-    {'name' =>'Flora', 'email'  => 'Flora@flowers.sun'},
-    {'name' =>'Janelle', 'email' => 'asdxcv@rails.rails'},
-    {'name' =>'Tate', 'email'  => 'sdfxcv@sql.sql'},
-    {'name' =>'Jenna', 'email' => 'zxcvsdf@css.css'},
-    {'name' =>'Rozalin', 'email'  => 'sdfxcv@ruby.org'},
-    {'name' =>'Amaleta', 'email' => 'Aasdf@ruby,ruby'}
+    {'email' => 'Ilka@gnet.serve'},
+    {'email'  => 'Ervin@ydos.parse'},
+    {'email'  => 'Davey@darvey.dave'},
+     {'email' => 'Desmund@hesmund.centro'},
+    {'email'  => 'Flora@flowers.sun'},
+    {'email' => 'asdxcv@rails.rails'},
+    {'email'  => 'sdfxcv@sql.sql'},
+    {'email' => 'zxcvsdf@css.css'},
+    {'email'  => 'sdfxcv@ruby.org'},
+    {'email' => 'Aasdf@ruby,ruby'}
 ]
 
 names.each do |userDeats|
-    User.create(name:userDeats['name'], email:userDeats['email'])
-    p "#{userDeats["name"]} created with email: #{userDeats["email"]}"
+    User.create(username:userDeats['email'])
+    p "username created with email: #{userDeats["email"]}"
 end
