da7d1f938f97e8bed3a9e9f57c358b8cbd8b57e2
ikesh pack <packappsforyou@gmail.com>
2019-06-17 18:18:23 -0400

    Wired up user#update route

--- first_routes_and_controllers/app/controllers/user_controller.rb
+++ first_routes_and_controllers/app/controllers/user_controller.rb
@@ -14,9 +14,22 @@ class UserController < ApplicationController
     render plain: "created user: #{user.name} with id: #{user.id}", json: user
   end
 
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      render json: user
+    else
+      render json: user.errors.full_messages, status: 422
+    end
+  end
+
   private
 
-  # def user_params
-  #   User.new(params.require(:user), permit(:name))
+  # def validate_name(name)
+  #   name.is_a?(String)
   # end
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
--- first_routes_and_controllers/config/routes.rb
+++ first_routes_and_controllers/config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get "/users", to: "user#index"
   get "/users/:id", to:"user#show"
   post "/users", to: "user#create"
+  patch "/users/:id", to: "user#update"
 end
